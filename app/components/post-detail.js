c93af729f9664dbbedb44c0bb8f0651c5665097b
doohinkus <doohinkus@gmail.com>
2017-01-11 16:03:59 -0800

    add editing to separate route.

--- app/components/post-detail.js
+++ app/components/post-detail.js
@@ -2,15 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    savePost() {
-      var params = {
-        author: this.get('author'),
-        title: this.get('title'),
-        image: this.get('image'),
-        body: this.get('body')
-      };
-      this.set('addNewPost', false);
-      this.sendAction('savePost', params);
-    }
+    update(post) {
+     var params = {
+       author: this.get('author'),
+       title: this.get('title'),
+       image: this.get('image'),
+       body: this.get('body')
+     };
+     this.sendAction('update', post, params);
+   }
  }
 });
--- app/routes/post.js
+++ app/routes/post.js
@@ -14,6 +14,10 @@ export default Ember.Route.extend({
      post.save();
      this.transitionTo('index');
    },
+   destroyPost(post) {
+    post.destroyRecord();
+    this.transitionTo('index');
+  },
    savePost(params) {
     var newPost = this.store.createRecord('post', params);
     newPost.save();
